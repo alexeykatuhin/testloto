6c3f875a7243f02f401a749899e7a60ad1714d12
alexeykatuhin <alexeykatuhin@gmail.com>
2019-02-14 20:53:51 +0300

    first commit

---
+++ WebApi/Concrete/EfContactRepository.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApi.Models;
+
+namespace WebApi.Concrete
+{
+    public class EfContactRepository 
+    {
+        ContactEntities _cntx = new ContactEntities();
+        public IEnumerable<Person> Persons => _cntx.Person;
+
+        public IEnumerable<Contacts> Contacts => _cntx.Contacts;
+
+        public void AddPerson(Person person)
+        {
+            _cntx.Person.Add(person);
+            _cntx.SaveChanges();
+        }
+
+        public void AddContacts(Contacts contact)
+        {
+            _cntx.Contacts.Add(contact);
+            _cntx.SaveChanges();
+        }
+
+        public void SaveContacts(Contacts contact)
+        {
+            Contacts dbEntry = _cntx.Contacts.Find(contact.Id);
+            if (dbEntry != null)
+            {
+                dbEntry.PersonId = contact.PersonId;
+                dbEntry.NameContact = contact.NameContact;
+                dbEntry.ValContact = contact.ValContact;
+            }
+            _cntx.SaveChanges();
+
+        }
+        public void SavePerson(Person person)
+        {
+            Person dbEntry = _cntx.Person.Find(person.Id);
+            if (dbEntry != null)
+            {
+                dbEntry.Name = person.Name;
+                dbEntry.BirthDate = person.BirthDate;
+                dbEntry.Comment = person.Comment;
+            }
+            _cntx.SaveChanges();
+
+        }
+
+        public Person DeletePerson(int Id)
+        {
+            Person dbEntry = _cntx.Person.Find(Id);
+            if (dbEntry != null)
+            {
+                _cntx.Person.Remove(dbEntry);
+                var list = _cntx.Contacts.Where(x => x.PersonId == Id);
+                foreach (var item in list)
+                {
+                    _cntx.Contacts.Remove(item);
+                }
+
+
+                _cntx.SaveChanges();
+
+            }
+            return dbEntry;
+        }
+
+        public Contacts DeleteContacts(int Id)
+        {
+            Contacts dbEntry = _cntx.Contacts.Find(Id);
+            if (dbEntry != null)
+            {
+                _cntx.Contacts.Remove(dbEntry);
+                _cntx.SaveChanges();
+            }
+            return dbEntry;
+        }
+
+    }
+}
---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Concrete;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private EfContactRepository _repo;
+
+     
+        public ContactController()
+
+        {
+            _repo = new EfContactRepository();
+        }
+
+
+        //получаем всех людей
+        [HttpGet]
+        [Route("api/person")]
+        public IEnumerable<Person> GetPerson()
+
+        {
+            return _repo.Persons;
+ 
+        }
+
+        //добавляем человека
+        [HttpPost]
+        [Route("api/person")]
+        public void AddPerson([FromBody]Person person)
+        {
+            _repo.AddPerson(person);
+        }
+
+        //редактируем человека
+        [HttpPut]
+        [Route("api/person")]
+        public void EditPerson([FromBody]Person person)
+        {
+            _repo.SavePerson(person);
+        }
+
+        //удаляем человека
+        [HttpDelete]
+        [Route("api/person/{id}")]
+        public void DeletePerson(int id)
+        {
+            _repo.DeletePerson(id);
+        }
+
+
+
+
+        //получаем контакт по id person
+        [HttpGet]
+        [Route("api/contacts/{id}")]
+        public IEnumerable<Contacts> GetContacts(int id)
+        {
+            return _repo.Contacts.Where(x => x.PersonId == id);
+        }
+
+
+
+        //добавляем контакт
+        [HttpPost]
+        [Route("api/contacts/{id}")]
+        public void AddContact(int id, [FromBody]ContactInfo contact)
+        {
+            _repo.AddContacts(new Contacts()
+            {
+                NameContact = contact.Name,
+                ValContact = contact.Value,
+                PersonId = id
+            });
+
+        }
+
+        //редактируем контакт
+        [HttpPut]
+        [Route("api/contact")]
+        public void EditContact([FromBody]Contacts contact)
+        {
+            _repo.SaveContacts(contact);
+        }
+
+        //удаляем контакт
+        [HttpDelete]
+        [Route("api/contact/{id}")]
+        public void DeleteContact(int id)
+        {
+            _repo.DeleteContacts(id);
+        }
+    }
+}
